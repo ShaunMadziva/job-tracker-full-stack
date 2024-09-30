bf22f5e919abb1fee68613781f565564b21839a3
Shaun <shaunmadziva@gmail.com>
2024-09-30 13:38:30 +0100

    home page table view

---
+++ client/src/components/JobApplicationsTable.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+const API = location.href.includes("localhost") ? "http://localhost:8080" : "https://[ProjectName].onrender.com";
+
+export default function JobApplicationsTable() {
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    fetchApplications();
+  }, []);
+
+  const fetchApplications = async () => {
+    try {
+      const response = await fetch(`${API}/job-applications`);
+      if (!response.ok) {
+        throw new Error(`Response was not ok ${response}`);
+      }
+      const data = await response.json();
+      setApplications(data);
+    } catch (error) {
+      console.error("Error fetching job applications:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Job Applications</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Job Title</th>
+            <th>Company Name</th>
+            <th>Date Applied</th>
+            <th>Interesting Fact</th>
+            <th>Brand Value 1</th>
+            <th>Brand Value 2</th>
+            <th>Brand Value 3</th>
+            <th>Cover Letter</th>
+            <th>Outcome</th>
+            <th>Follow-up Date</th>
+            <th>Followed Up</th>
+          </tr>
+        </thead>
+        <tbody>
+          {applications.map(
+            ({
+              id,
+              job_title,
+              company_name,
+              date_applied,
+              interesting_fact,
+              brand_value_1,
+              brand_value_2,
+              brand_value_3,
+              cover_letter_included,
+              outcome,
+              follow_up_date,
+              followed_up,
+            }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{job_title}</td>
+                <td>{company_name}</td>
+                <td>{date_applied}</td>
+                <td>{interesting_fact}</td>
+                <td>{brand_value_1}</td>
+                <td>{brand_value_2}</td>
+                <td>{brand_value_3}</td>
+                <td>{cover_letter_included}</td>
+                <td>{outcome}</td>
+                <td>{follow_up_date}</td>
+                <td>{followed_up}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
